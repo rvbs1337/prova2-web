cbc7271cad9efe3ba797d329fdfdc843fa57f3af
Rubens <rubenspereira9@hotmail.com>
2024-05-26 23:49:34 -0300

    iniciando querystring

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
 document.addEventListener("DOMContentLoaded", (e) => {
   // e.preventDefault()
+  carregaNoticias();
 });
 
 function openDialog() {
-  console.log(2);
   document.querySelector("#dialogFiltro").showModal();
 }
 
 function closeDialog() {
   document.querySelector("#dialogFiltro").close();
+}
+
+function generateItens(data) {
+  const main = document.querySelector("main");
+  const ul = document.createElement("ul");
+  main.appendChild(ul);
+
+  data.items.forEach((e) => {
+    console.log(e);
+    const li = document.createElement("li");
+    li.textContent = e.titulo;
+
+    ul.appendChild(li);
+  });
+}
+
+function closeDialogApply() {
+  const form = document.querySelector("#filtro");
+
+  // location.assign("?data");
+  Array.from(form.elements).forEach((input) => {
+    console.log(input.value);
+  });
+}
+
+function getSearchParams() {
+  // Early return -> Caso location search, não faz nada.
+  if (!location.search) {
+    return;
+  }
+
+  // URLSearchParams é uma classe que facilita a manipulação de query strings
+  const urlSearchParams = new URLSearchParams(location.search);
+}
+
+async function carregaNoticias() {
+  try {
+    const data = await fetch(
+      `http://servicodados.ibge.gov.br/api/v3/noticias/?qtd=10`
+    );
+
+    const jsonData = await data.json();
+    console.log(jsonData);
+    generateItens(jsonData);
+  } catch (error) {
+    console.error(error);
+  }
 }
