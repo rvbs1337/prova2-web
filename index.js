300699bd316b5fbdd0b160dbdf7d8f60b5b0d514
Rubens <rubenspereira9@hotmail.com>
2024-05-30 15:34:41 -0300

    filtro funcionando

--- index.js
+++ index.js
@@ -13,45 +13,78 @@ function closeDialog() {
 
 function generateItens(data) {
   const main = document.querySelector("main");
+  main.innerHTML = "";
   const ul = document.createElement("ul");
   main.appendChild(ul);
 
   data.items.forEach((e) => {
-    console.log(e);
+    console.log(JSON.parse(e.imagens));
     const li = document.createElement("li");
-    li.textContent = e.titulo;
-
+    const img = document.createElement("img");
+    const h2 = document.createElement("h2");
+    img.src =
+      "https://agenciadenoticias.ibge.gov.br/" +
+      JSON.parse(e.imagens).image_intro;
+    h2.textContent = e.titulo;
+    li.appendChild(img);
+    li.appendChild(h2);
     ul.appendChild(li);
   });
 }
 
 function closeDialogApply() {
-  const form = document.querySelector("#filtro");
+  const qtd = document.querySelector("#quantidade");
+  const tipo = document.querySelector("#tipo");
+  const de = document.querySelector("#de");
+  const ate = document.querySelector("#ate");
 
-  // location.assign("?data");
-  Array.from(form.elements).forEach((input) => {
-    console.log(input.value);
-  });
+  const url = new URL(window.location);
+  url.searchParams.set("qtd", qtd.value);
+  url.searchParams.set("tipo", tipo.value);
+  url.searchParams.set("de", de.value);
+  url.searchParams.set("ate", ate.value);
+  window.history.pushState({}, "", url);
+
+  closeDialog();
+  carregaNoticias();
 }
 
 function getSearchParams() {
-  // Early return -> Caso location search, não faz nada.
   if (!location.search) {
     return;
   }
 
-  // URLSearchParams é uma classe que facilita a manipulação de query strings
   const urlSearchParams = new URLSearchParams(location.search);
+  var params = {};
+  params.qtd = urlSearchParams.get("qtd");
+  params.tipo = urlSearchParams.get("tipo");
+  params.de = urlSearchParams.get("de");
+  params.ate = urlSearchParams.get("ate");
+
+  return params;
 }
 
 async function carregaNoticias() {
   try {
+    var params = getSearchParams();
+    console.log(params);
+    var linkParam = `?qtd=${params.qtd}`;
+    if (params.tipo) {
+      linkParam += `&tipo=${params.tipo}`;
+    }
+    if (params.de) {
+      linkParam += `&de=${params.de}`;
+    }
+    if (params.ate) {
+      linkParam += `&ate=${params.ate}`;
+    }
+
     const data = await fetch(
-      `http://servicodados.ibge.gov.br/api/v3/noticias/?qtd=10`
+      `http://servicodados.ibge.gov.br/api/v3/noticias/${linkParam}`
     );
 
     const jsonData = await data.json();
-    console.log(jsonData);
+
     generateItens(jsonData);
   } catch (error) {
     console.error(error);
